45b0438f0421677cbaf8eb7582ea2bf3e354e4bb
Richard Tan <chardos@gmail.com>
2016-05-01 17:54:47 +1000

    Destructuring!

--- translation.js
+++ translation.js
@@ -5,25 +5,9 @@ var parseString = require('xml2js').parseString;
 
 function translate(text, chatId, bot){
   detectLanguage(text);
+  var params = {text,chatId, bot};
+  translateText(params);
 
-  var options = {
-    hostname: 'translate.yandex.net',
-    port: 443,
-    path: '/api/v1.5/tr.json/translate?key=' + apiKey +
-    '&text=' + escape(text) +
-    '&lang=en-de' +
-    '&format=plain'
-  };
-  var req = https.request(options, (res) => {
-    console.log('statusCode: ', res.statusCode);
-
-    res.on('data', (d) => {
-      var text = JSON.parse(d.toString()).text[0];
-      console.log(text);
-      bot.sendMessage(chatId, text)
-    });
-  });
-  req.end();
 }
 
 function detectLanguage(text){
@@ -34,22 +18,37 @@ function detectLanguage(text){
     '&text=' + escape(text)
   };
   var req = https.request(options, (res) => {
-    console.log('statusCode: ', res.statusCode);
-
     res.on('data', (data) => {
       var lang;
       parseString(data, (err, result) => {
         lang = result.DetectedLang.$.lang;
       });
       console.log('lang: ' + lang);
-
-
-
     });
   });
   req.end();
 }
 
+function translateText(params){
+  var options = {
+    hostname: 'translate.yandex.net',
+    port: 443,
+    path: '/api/v1.5/tr.json/translate?key=' + apiKey +
+    '&text=' + escape(params.text) +
+    '&lang=en-de' +
+    '&format=plain'
+  };
+  var req = https.request(options, (res) => {
+    console.log('statusCode: ', res.statusCode);
+
+    res.on('data', (d) => {
+      var text = JSON.parse(d.toString()).text[0];
+      console.log(text);
+      params.bot.sendMessage(params.chatId, text)
+    });
+  });
+  req.end();
+}
 
 
 module.exports = translate;
