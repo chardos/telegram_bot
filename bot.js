c1a5a9f8b4d6b6fa7cde468b01831d03ba0b04ff
Richard Tan <chardos@gmail.com>
2016-05-01 17:40:01 +1000

    Translation.js file split

--- bot.js
+++ bot.js
@@ -1,8 +1,7 @@
 require('./env.js')
-var apiKey = process.env['YANDEX_API_KEY'];
+var translate = require('./translation.js')
 var token = process.env['TELEGRAM_BOT_TOKEN'];
-var https = require('https');
-var parseString = require('xml2js').parseString;
+
 
 var Bot = require('node-telegram-bot-api'),
     bot = new Bot(token, { polling: true });
@@ -12,51 +11,11 @@ console.log('bot server started...');
 
 bot.onText(/^\/translate (.+)$/, function (msg, match) {
   var text = match[1];
-  detectLanguage(text)
-  // translateText(text, msg)
+  var chatId = msg.chat.id;
+  // detectLanguage(text)
+  translate(text, chatId, bot)
 });
 
-function translateText(text, msg){
-  var options = {
-    hostname: 'translate.yandex.net',
-    port: 443,
-    path: '/api/v1.5/tr.json/translate?key=' + apiKey +
-    '&text=' + escape(text) +
-    '&lang=en-de' +
-    '&format=plain'
-  };
-  var req = https.request(options, (res) => {
-    console.log('statusCode: ', res.statusCode);
-
-    res.on('data', (d) => {
-      var text = JSON.parse(d.toString()).text[0];
-      console.log(text);
-      bot.sendMessage(msg.chat.id, text)
-    });
-  });
-  req.end();
-}
-function detectLanguage(text){
-  var options = {
-    hostname: 'translate.yandex.net',
-    port: 443,
-    path: '/api/v1.5/tr/detect?key=' + apiKey +
-    '&text=' + escape(text)
-  };
-  var req = https.request(options, (res) => {
-    console.log('statusCode: ', res.statusCode);
-
-    res.on('data', (data) => {
-      var lang;
-      parseString(data, (err, result) => {
-        lang = result.DetectedLang.$.lang;
-      });
-      console.log('lang: ' + lang);
 
 
-
-    });
-  });
-  req.end();
-}
-detectLanguage('Wann kommen Sie vorbei?')
+// detectLanguage('Wann kommen Sie vorbei?')
--- translation.js
+++ translation.js
@@ -1,7 +1,55 @@
-function translate(){
-  
+var Promise = require("bluebird");
+var https = require('https');
+var apiKey = process.env['YANDEX_API_KEY'];
+var parseString = require('xml2js').parseString;
+
+function translate(text, chatId, bot){
+  detectLanguage(text);
+
+  var options = {
+    hostname: 'translate.yandex.net',
+    port: 443,
+    path: '/api/v1.5/tr.json/translate?key=' + apiKey +
+    '&text=' + escape(text) +
+    '&lang=en-de' +
+    '&format=plain'
+  };
+  var req = https.request(options, (res) => {
+    console.log('statusCode: ', res.statusCode);
+
+    res.on('data', (d) => {
+      var text = JSON.parse(d.toString()).text[0];
+      console.log(text);
+      bot.sendMessage(chatId, text)
+    });
+  });
+  req.end();
+}
+
+function detectLanguage(text){
+  var options = {
+    hostname: 'translate.yandex.net',
+    port: 443,
+    path: '/api/v1.5/tr/detect?key=' + apiKey +
+    '&text=' + escape(text)
+  };
+  var req = https.request(options, (res) => {
+    console.log('statusCode: ', res.statusCode);
+
+    res.on('data', (data) => {
+      var lang;
+      parseString(data, (err, result) => {
+        lang = result.DetectedLang.$.lang;
+      });
+      console.log('lang: ' + lang);
+
+
+
+    });
+  });
+  req.end();
 }
 
 
 
-module.exports.translate = translate;
+module.exports = translate;
